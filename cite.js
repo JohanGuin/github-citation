59265af8d18f9b69604721cb54f26ed656ecea99
JohanGuin <johan.guin@gmail.com>
2021-08-30 12:20:15 +0200

    version 1 début du programme

---
+++ cite.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const citations = [
+  {
+    texte: "Si vous voulez vivre une vie heureuse, attachez-la à un but, et non pas à des personnes ou à des choses.",
+    auteur: "Albert Einstein",
+    categorie: ["bonheur"],
+    image: [],
+  },
+  {
+    texte: "En plein coeur de toute difficulté se cache une possibilité.",
+    auteur: "Albert Einstein",
+    categorie: ["courage", "sagesse"],
+    image: [],
+  },
+  {
+    texte: "La seule chose qui fait que la vie mérite d'être vécue, c'est de vivre pour les autres.",
+    auteur: "Albert Einstein",
+    categorie: ["bonheur", "morale", "sagesse"],
+    image: [],
+  },
+  {
+    texte: "Ce qui faît la vrai valeur d'un être humain, c'est de s'être délivré de son petit moi.",
+    auteur: "Albert Einstein",
+    categorie: [""],
+    image: [],
+  },
+  {
+    texte: "Je dois être prêt à renoncer à ce que je suis pour devenir ce que je serai.",
+    auteur: "Albert Einstein",
+    categorie: ["bonheur", "courage"],
+    image: [],
+  },
+  {
+    texte: "N'essayez pas de devenir un homme qui a du succès, essayez de devenir un homme qui a de la valeur.",
+    auteur: "Albert Einstein",
+    categorie: ["sagesse"],
+    image: [],
+  },
+  {
+    texte: "Rare est le nombre de ceux qui regardent avec leurs propres yeux et qui éprouvent avec leur propre sensibilité",
+    auteur: "Albert Einstein",
+    categorie: ["sensibilité"],
+    image: [],
+  },
+  {
+    texte: "Tout ce que est vraiment grand et inspiré a été réalisé par des individus travaillant librement.",
+    auteur: "Albert Einstein",
+    categorie: ["liberté"],
+    image: [],
+  },
+];
