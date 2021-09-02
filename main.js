35efe90e64f8e2e9f0e3cbed626197bad18e5a2f
JohanGuin <johan.guin@gmail.com>
2021-09-02 23:10:47 +0200

    fin de l'algo et ajout de citations

--- main.js
+++ main.js
@@ -7,23 +7,9 @@
 const citation = document.getElementById("cite");
 const auteur = document.getElementById("auteur");
 const boutons = document.querySelectorAll("button");
-let boolTous = true;
-let boolBon = false;
-let boolCou = false;
-let boolLib = false;
-let boolSag = false;
-let boolMor = false;
-let intervalId = null;
 
 // tableau ou seront entré toutes les citations possibles en fonction du choix de l'utilisateur
 let choixUtilisateur = [];
-
-// fonction se lançant au début du programme pour afficher automatiquement une citation aléatoire
-(function() {
-  const debutRandomCitation = Math.floor(Math.random()*citations.length);
-  cite.textContent = citations[debutRandomCitation].texte;
-  auteur.textContent = citations[debutRandomCitation].auteur;
-})();
 
 // Remplissage des options du tableau suite aux choix de l'utilisateur
 let remplirTab = (mot) => {
@@ -38,7 +24,12 @@ let remplirTab = (mot) => {
 }
 
 let lectureCitations = () => {
-  
+  const debutRandomCitation = Math.floor(Math.random()*choixUtilisateur.length);
+  cite.textContent = choixUtilisateur[debutRandomCitation].texte;
+  auteur.textContent = choixUtilisateur[debutRandomCitation].auteur;
+  cite.style.animation = "afficheCitation 5s";
+  auteur.style.animation = "afficheCitation 5s";
+  console.log(debutRandomCitation);
 }
 
 /*Fonction qui permet le choix utilisateur et lance les citations*/
@@ -51,39 +42,22 @@ function buttonChoixUser(){
   mor.classList.remove("button-true");
   this.classList.add("button-true");
   if(this.id === "tous"){
-    boolTous = true;
-    boolBon = false; boolCou = false; boolLib = false;
-    boolSag = false; boolMor = false;
     choixUtilisateur = [...citations];
   }else if(this.id === "bon"){
-    boolBon = true;
-    boolTous = false; boolCou = false; boolLib = false;
-    boolSag = false; boolMor = false;
     remplirTab("bonheur");
   }else if(this.id === "cou"){
-    boolCou = true;
-    boolBon = false; boolTous = false; boolLib = false;
-    boolSag = false; boolMor = false;
     remplirTab("courage");
   }else if(this.id === "lib"){
-    boolLib = true;
-    boolBon = false; boolCou = false; boolTous = false;
-    boolSag = false; boolMor = false;
     remplirTab("liberte");
   }else if(this.id === "sag"){
-    boolSag = true;
-    boolBon = false; boolCou = false; boolLib = false;
-    boolTous = false; boolMor = false;
     remplirTab("sagesse");
   }else{
-    boolMor = true;
-    boolBon = false; boolCou = false; boolLib = false;
-    boolSag = false; boolTous = false;
     remplirTab("morale");
   }
   lectureCitations();
+  cite.style.animation = "afficheCitation 5s alternate";
+  auteur.style.animation = "afficheCitation 5s alternate";
 }
-
 
 tous.addEventListener("click", buttonChoixUser);
 bon.addEventListener("click", buttonChoixUser);
