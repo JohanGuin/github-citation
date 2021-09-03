33644c91fa8675ad425bdb216bb68b3ea580ca45
JohanGuin <johan.guin@gmail.com>
2021-09-03 18:09:43 +0200

    mise en place d'images de fond aleatoire en rapport avec les citations

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
   Programme affichant des citations de personnes célèbres avec quelques options pour l'utilisateur qui pourra choisir un thème.
 */
 
-// const citation = document.getElementById("cite");
-// const auteur = document.getElementById("auteur");
 const boutons = document.querySelectorAll("button");
 const conteneurCitation = document.getElementById("conteneur-citation");
 
@@ -26,14 +24,37 @@ let remplirTab = (mot) => {
 
 let lectureCitations = () => {
   const debutRandomCitation = Math.floor(Math.random()*choixUtilisateur.length);
+  let longueur = choixUtilisateur[debutRandomCitation].image.length;
+  const randomImage = Math.floor(longueur - 1);
   let cite = document.createElement("blockquote");
   let auteur = document.createElement("div");
   let photo = new Image();
+  let fond = new Image();
+  let rImg = choixUtilisateur[debutRandomCitation].image[randomImage]
 
   cite.setAttribute("id", "cite");
   auteur.setAttribute("id", "auteur");
   photo.setAttribute("id", "photo");
-  photo.src = "assets/Albert_Einstein.jpg";
+  fond.setAttribute("id", "fond");
+  console.log(rImg);
+
+  fond.src = `assets/${rImg}`;
+
+  if(choixUtilisateur[debutRandomCitation].auteur === "Albert Einstein"){
+    photo.src = "assets/Albert_Einstein.jpg";
+  }else if(choixUtilisateur[debutRandomCitation].auteur === "Malcolm X"){
+    photo.src = "assets/Malcolm_X.jpg";
+  }else if(choixUtilisateur[debutRandomCitation].auteur === "Victor Hugo"){
+    photo.src = "assets/Victor_Hugo.jpg";
+  }else if(choixUtilisateur[debutRandomCitation].auteur === "Martin Luther King"){
+    photo.src = "assets/Martin_Luther_King.jpg";
+  }else if(choixUtilisateur[debutRandomCitation].auteur === "Aldous Huxley"){
+    photo.src = "assets/Aldous_Huxley.jpg";
+  }else if(choixUtilisateur[debutRandomCitation].auteur === "Aristote"){
+    photo.src = "assets/Aristote.jpg";
+  }
+
+  conteneurCitation.append(fond);
   conteneurCitation.append(cite);
   conteneurCitation.append(auteur);
   conteneurCitation.append(photo);
@@ -42,14 +63,17 @@ let lectureCitations = () => {
   cite.style.animation = "afficheCitation 5s";
   auteur.style.animation = "afficheCitation 5s";
   photo.style.animation = "afficheCitation 5s";
+  fond.style.animation = "afficheFond 5s";
   boutons.forEach((bouton) => {
     bouton.addEventListener("click", () => {
       cite.remove();
       auteur.remove();
       photo.remove();
+      fond.remove();
     });
   });
 }
+
 
 /*Fonction qui permet le choix utilisateur et lance les citations*/
 function buttonChoixUser(){
@@ -59,6 +83,8 @@ function buttonChoixUser(){
   lib.classList.remove("button-true");
   sag.classList.remove("button-true");
   mor.classList.remove("button-true");
+  rev.classList.remove("button-true");
+  sen.classList.remove("button-true");
   this.classList.add("button-true");
   if(this.id === "tous"){
     choixUtilisateur = [...citations];
@@ -70,9 +96,14 @@ function buttonChoixUser(){
     remplirTab("liberte");
   }else if(this.id === "sag"){
     remplirTab("sagesse");
-  }else{
+  }else if(this.id === "mor"){
     remplirTab("morale");
+  }else if(this.id === "rev"){
+    remplirTab("reve");
+  }else{
+    remplirTab("sensibilite");
   }
+
   lectureCitations();
 }
 
@@ -82,3 +113,5 @@ cou.addEventListener("click", buttonChoixUser);
 lib.addEventListener("click", buttonChoixUser);
 sag.addEventListener("click", buttonChoixUser);
 mor.addEventListener("click", buttonChoixUser);
+rev.addEventListener("click", buttonChoixUser);
+sen.addEventListener("click", buttonChoixUser);
